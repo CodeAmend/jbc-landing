1c9e6d6cbf8c5a5bebdc76b0f057353d01a7117f
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-07-10 10:30:35 -0500

    added svg lung animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,44 @@
-
-var jbc = document.getElementById('jbc');
-var overlay = document.getElementById('jbc_overlay');
-
-// jbc.addEventListener('mouseover', showOverlay );
-//
-// function showOverlay(e) {
-//   var name = e.target.className;
-//   overlay.innerText = name;
-// }
-
-function animateTitle() {
-
-}
-
-
 var draw = SVG('lungs').size(686, 635);
+
+
+// NAV buttons
+var blog = document.getElementById('blog');
+var funds = document.getElementById('funds');
+var nav = document.getElementById('nav');
+
+
 
 // lungPath and pipePath in svgPath.js must be imported in HTML
 var lungs = draw.path(lungPath);
 var pipes = draw.path(pipePath);
+var lungGroup = draw.group();
 
-lungs.attr({
-  fill: '#454545'
-}).translate(0, 125);
+lungs.translate(0, 123);
 
-pipes.attr({
-  fill: '#A0A0A0'
-})
+function lungDefault() {
+  lungs.stop(true, true)
+  pipes.stop(true, true)
+  lungs.animate().attr({
+    fill: '#454545'
+  });
+
+  pipes.animate().attr({
+    fill: '#A0A0A0'
+  });
+}
+
+function blueFlash() {
+  lungs.stop(true);
+  pipes.stop(true)
+  lungs.animate({ ease: '<>', duration: 400 }).attr({
+    fill: '#82b9ff'
+  });
+
+  pipes.animate().attr({
+    fill: '#50a0ff'
+  });
+}
+
+lungDefault();
+funds.addEventListener('mouseover', blueFlash);
+funds.addEventListener('mouseout', lungDefault);
